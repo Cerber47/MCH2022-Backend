e72ec0b5821af24a2ba8f580be500ebac1b35642
reability <4reability@gmail.com>
2022-06-12 00:20:32 +0300

    app fix 3

--- Source/App.py
+++ Source/App.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask import Blueprint
 from pymongo import MongoClient
-from Source.Routers.Router import AppRouter
-from Source.Controllers.Partners import PartnersController
-from Source.Models.Partners import PartnersModel
+from Routers.Router import AppRouter
+from Controllers.Partners import PartnersController
+from Models.Partners import PartnersModel
 
 
 PARTNERSBP = "/patners"
@@ -12,7 +12,10 @@ PARTNERSBP = "/patners"
 if __name__ == "__main__":
     print("STARTING AN APPLICATION")
     app = Flask(__name__)
-    router = AppRouter(Blueprint(PARTNERSBP, __name__))
+
+    bp = Blueprint(PARTNERSBP, __name__)
+    app.register_blueprint(bp)
+    router = AppRouter(bp)
     client = MongoClient("localhost", 27017, username="username", password="password")
     db = client.db
 
